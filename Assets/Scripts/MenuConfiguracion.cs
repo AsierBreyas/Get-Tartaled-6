20464bbeb3bf614513295c6a4017bb9e98224f5c
Endika Agirre <josu0805@gmail.com>
2025-01-09 11:49:16 +0100

    configuracion audio funcional

--- Assets/Scripts/MenuConfiguracion.cs
+++ Assets/Scripts/MenuConfiguracion.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MenuConfiguracion : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     public void SetVolume (float volume)
     {
-        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume); 
     }
-
     public void Atras()
     {
         SceneManager.LoadScene("MenuInicio");
