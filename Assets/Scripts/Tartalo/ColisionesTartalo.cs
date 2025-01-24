b0a5a0c65706eccdcd3d461f6e4e77664b423a68
AsierBreyas <asierbreyas111@gmail.com>
2025-01-24 13:52:18 +0100

    El juego copila

--- Assets/Scripts/Tartalo/ColisionesTartalo.cs
+++ Assets/Scripts/Tartalo/ColisionesTartalo.cs
@@ -5,14 +5,14 @@ public class ColisionesTartalo : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         Debug.Log("PUM quemao");
-        FindFirstObjectByType<ControlesTartalo>().TakeDamage(0.33f);
+        FindFirstObjectByType<ControlesTartalo>().TakeDamage(0.01f);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Lobo")
         {
-            FindFirstObjectByType<ControlesTartalo>().TakeDamage(5f);
+            FindFirstObjectByType<ControlesTartalo>().TakeDamage(1f);
         }
     }
 }
