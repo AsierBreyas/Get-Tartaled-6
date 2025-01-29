5343fe9bf5d6bea6e389af4540e7550d5a786fc4
Endika Agirre <josu0805@gmail.com>
2025-01-29 10:36:21 +0100

    profile data

---
+++ Assets/Scripts/Sistemas/Guardado/ProfileData.cs
@@ -0,0 +1,32 @@
+public class ProfileData
+{
+    // Datos perfil
+    public string fileName;
+    public string name;
+    public bool newGame;
+
+    // Datos a guardar
+    public float currentHealth;
+    public float x, y, z;
+
+    // Misiones aqui
+
+    public ProfileData()
+    {
+        this.fileName = "None.xml";
+        this.name = "None";
+        this.newGame = false;
+
+        this.y = this.x = this.z = 0;
+    }
+    
+    public ProfileData (string name, bool newGame, float x, float y, float z)
+    {
+        this.fileName = name.Replace(" ", "_") + ".xml";
+        this.name = name;
+        this.newGame = newGame;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
