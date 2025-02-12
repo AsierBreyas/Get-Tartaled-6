2e3b6fbe2dc262b34a48c60c82bcbadfd2ef3478
Josu Gonzalez <josu0805@gmail.com>
2025-02-12 13:56:27 +0100

    localizacion menu inicio y config

---
+++ Assets/Scripts/Multilanguage.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Assets.SimpleLocalization.Scripts;
+
+public class Multilanguage : MonoBehaviour
+{
+    private void Awake()
+    {
+        LocalizationManager.Read();
+
+        LocalizationManager.Language = "Espaniol";
+    }
+}
